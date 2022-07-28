dcce224cd5943a6a5bd663d424e4de3e1e603a16
AtylaDaSilva <atylamendes97@gmail.com>
2022-07-28 16:57:24 -0300

    implemented handleChange listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    form: [
+      { id: "rg", value: "" },
+      { id: "cpf", value: "" },
+      { id: "phone", value: "" },
+      { id: "email", value: "" },
+    ]
+  }
+
+  handleChange = (event) => { 
+    let newState = this.state.form;
+    
+    for (let element of newState) { 
+      if (element.id === event.target.id) { 
+        element.value = event.target.value;
+        this.setState({ form: newState });
+      }
+    }
+
+    console.table(newState);
+  }
+
   
   render() { 
     return (
@@ -10,29 +32,37 @@ class App extends Component {
             <legend>Form Validation With RegExp</legend>
             <div className="rgForm">
               <fieldset>
-                <legend><label htmlFor="rg">RG: </label></legend>
-                <input type="text" name="$rg" id="rg" placeholder="some text" />
+                <legend><label htmlFor={ this.state.form[0].id }>RG: </label></legend>
+                <input type="text" name="$rg" id={this.state.form[0].id} placeholder="some text"
+                  onChange={this.handleChange}
+                />
                 <button type="button">Validate</button>
               </fieldset>
             </div>
             <div className="cpfForm">
               <fieldset>
-                <legend><label htmlFor="cpf">CPF: </label></legend>
-                <input type="text" name="$cpf" id="cpf" placeholder="some more text" />
+                <legend><label htmlFor={ this.state.form[1].id }>CPF: </label></legend>
+                <input type="text" name="$cpf" id={this.state.form[1].id} placeholder="some more text"
+                  onChange={this.handleChange}
+                />
                 <button type="button">Validate</button>
               </fieldset>
             </div>
             <div className="phoneForm">
               <fieldset>
-                <legend><label htmlFor="phone">Phone: </label></legend>
-                <input type="tel" name="$phone" id="phone" placeholder="Your phone goes here" />
+                <legend><label htmlFor={ this.state.form[2].id }>Phone: </label></legend>
+                <input type="tel" name="$phone" id={this.state.form[2].id} placeholder="Your phone goes here"
+                  onChange={this.handleChange}
+                />
                 <button type="button">Validate</button>
               </fieldset>
             </div>
             <div className="emailForm">
               <fieldset>
-                <legend><label htmlFor="email">Email: </label></legend>
-                <input type="email" name="$email" id="email" placeholder="some even more text" />
+                <legend><label htmlFor={ this.state.form[3].id }>Email: </label></legend>
+                <input type="email" name="$email" id={this.state.form[3].id} placeholder="some even more text"
+                  onChange={this.handleChange}
+                />
                 <button type="button">Validate</button>
               </fieldset>
             </div>
