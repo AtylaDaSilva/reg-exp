9245226400260fe1ac5685e66957cf482f7f2c7c
AtylaDaSilva <atylamendes97@gmail.com>
2022-07-28 22:20:33 -0300

    implemented validation logic on all input fields

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,43 @@ class App extends Component {
   validateRg = () => { 
     const rg = this.state.form[0].value;
 
-    if (!rg) { 
-      console.log("Please, insert a valid RG");
-      return false;
-    }
-
-    console.log("RG = ", rg);
-
     const regExp = new RegExp(/^\d{10}\s?-?\s?\d{1}$/);
     const result = rg.match(regExp);
 
-    console.log("Match = ", result[0]);
+    return result;
+  }
+
+  validateCpf = () => { 
+    /*Validate CPF */
+    const cpf = this.state.form[1].value;
+    
+    const regExp = new RegExp(/^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/);
+    const result = cpf.match(regExp);
+
+    return result;
+  }
+
+  validatePhone = () => { 
+    /*Validate Phone */
+    const phone = this.state.form[2].value;
+    
+    const regExp = new RegExp(/^9\.?\d{4}-?\d{4}$/);
+    const result = phone.match(regExp);
+
+    console.log(result);
+
+    return result;
+  }
+
+  validateEmail = () => { 
+    /*Validate Email */
+    const email = this.state.form[3].value;
+
+    const regExp = new RegExp(/^\w*@\w+\.\w{2,3}$/);
+    const result = email.match(regExp);
+
+    console.log(result);
+
     return result;
   }
 
@@ -48,41 +74,37 @@ class App extends Component {
             <div className="rgForm">
               <fieldset>
                 <legend><label htmlFor={this.state.form[0].id}>RG: </label></legend>
-                <input type="text" name="$rg" id={this.state.form[0].id} placeholder="some text"
+                <input type="text" name="$rg" id={this.state.form[0].id} placeholder="Ex.: 1234567890-1"
                   onChange={this.handleChange}
                 />
-                <button type="button"
-                  onClick={ this.validateRg }
-                >
-                  Validate
-                </button>
+                <button type="button" onClick={ this.validateRg }> Validate </button>
               </fieldset>
             </div>
             <div className="cpfForm">
               <fieldset>
                 <legend><label htmlFor={ this.state.form[1].id }>CPF: </label></legend>
-                <input type="text" name="$cpf" id={this.state.form[1].id} placeholder="some more text"
+                <input type="text" name="$cpf" id={this.state.form[1].id} placeholder="Ex.: 123.456.789-01"
                   onChange={this.handleChange}
                 />
-                <button type="button">Validate</button>
+                <button type="button" onClick={ this.validateCpf }>Validate</button>
               </fieldset>
             </div>
             <div className="phoneForm">
               <fieldset>
                 <legend><label htmlFor={ this.state.form[2].id }>Phone: </label></legend>
-                <input type="tel" name="$phone" id={this.state.form[2].id} placeholder="Your phone goes here"
+                <input type="tel" name="$phone" id={this.state.form[2].id} placeholder="Ex.: 9.1234-5678"
                   onChange={this.handleChange}
                 />
-                <button type="button">Validate</button>
+                <button type="button" onClick={ this.validatePhone }>Validate</button>
               </fieldset>
             </div>
             <div className="emailForm">
               <fieldset>
                 <legend><label htmlFor={ this.state.form[3].id }>Email: </label></legend>
-                <input type="email" name="$email" id={this.state.form[3].id} placeholder="some even more text"
+                <input type="email" name="$email" id={this.state.form[3].id} placeholder="Ex.: email@email.com"
                   onChange={this.handleChange}
                 />
-                <button type="button">Validate</button>
+                <button type="button" onClick={ this.validateEmail }>Validate</button>
               </fieldset>
             </div>
           </fieldset>
